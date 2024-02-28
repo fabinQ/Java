37049728055d71081fcb21343be1f075094211de
fabinQ <maciejfabinski@gmail.com>
2024-02-28 23:01:28 +0100

    Reformated repo

---
+++ Basic/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jak masz na imie?");
+        String imie = scanner.nextLine();
+
+        System.out.println("Ile masz lat?");
+        int wiek = scanner.nextInt();
+        System.out.println("Cześć "+ imie + "! Masz "+wiek+" lat.");
+    }
+}
